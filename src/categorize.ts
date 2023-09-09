109276daf742288e5731dceb116ff9acba40a374
dalton-mcphaden <dalton.mcphaden@vention.cc>
2023-09-09 15:28:53 -0400

    cleanup

--- src/categorize.ts
+++ src/categorize.ts
@@ -1,16 +1,27 @@
 import { RawGithubIssue } from "./models"
-
 const inquirer = require("inquirer")
 
-export async function inquire(rawIssue: RawGithubIssue) {
+const TICKET_CATEGORIES = ["frontend", "backend", "database", "devops", "other"]
+
+export async function inquire(rawIssue: RawGithubIssue): Promise<string> {
+  const { number, title, labels, milestone, created_at, closed_at } = rawIssue
+  const formattedLabels = labels.map(label => label.name).join(", ")
+
+  const promptMessage = `
+Categorize this issue:
+#${number} ${title}
+Labels: ${formattedLabels}
+Milestone: ${milestone?.title ?? "N/A"}
+Created at: ${created_at}
+Closed at: ${closed_at ?? "N/A"}
+`
+
   const response = await inquirer.prompt([
     {
       type: "list",
       name: "ticketCategory",
-      message: `Categorize this issue: \n#${rawIssue.number} ${rawIssue.title} \nLabels: ${rawIssue.labels
-        .map(label => label.name)
-        .join(", ")}\nMilestone: ${rawIssue.milestone?.title}\nCreated at: ${rawIssue.created_at}\nClosed at: ${rawIssue.closed_at}\n`,
-      choices: ["frontend", "backend", "database", "devops", "other"],
+      message: promptMessage,
+      choices: TICKET_CATEGORIES,
     },
   ])
 
--- src/main.ts
+++ src/main.ts
@@ -3,42 +3,65 @@ import { loadEnvironmentVariables } from "./utils"
 import { StoredGithubIssue, RawGithubIssue } from "./models"
 import { fetchGithubIssues } from "./issues"
 import { inquire } from "./categorize"
-import test from "node:test"
+
+async function getStoredIssues() {
+  const db = getConnection()
+  return db.get("githubIssues").value()
+}
+
+async function storeNewIssues(newIssues: StoredGithubIssue[]) {
+  const db = getConnection()
+  db.get("githubIssues")
+    .push(...newIssues)
+    .write()
+}
+
+async function categorizeNewIssues(storedIssues: StoredGithubIssue[], rawIssues: RawGithubIssue[]): Promise<StoredGithubIssue[]> {
+  const newIssues: StoredGithubIssue[] = []
+
+  for (const rawIssue of rawIssues) {
+    const existingIssue = storedIssues.find(storedIssue => storedIssue.id === rawIssue.id)
+    if (!existingIssue) {
+      const category = await inquire(rawIssue)
+      const categorizedIssue = { ...rawIssue, category } as StoredGithubIssue
+      newIssues.push(categorizedIssue)
+      console.log(`Issue ${rawIssue.number} categorized and stored`)
+    }
+  }
+
+  return newIssues
+}
+
+function countAndLogIssues(categorizedIssues: StoredGithubIssue[]) {
+  const categories = ["frontend", "backend", "database", "devops", "other"]
+  const counts: { [key: string]: number } = {}
+
+  for (const category of categories) {
+    counts[category] = categorizedIssues.filter(issue => issue.category === category).length
+    console.log(
+      `${category.charAt(0).toUpperCase() + category.slice(1)} issues: ${counts[category]} (${Math.round(
+        (counts[category] / categorizedIssues.length) * 100
+      )}%)`
+    )
+  }
+
+  console.log(`Total issues completed: ${categorizedIssues.length}`)
+}
 
 async function main() {
   loadEnvironmentVariables()
   createConnection()
 
-  const db = getConnection()
-
-  const storedIssues = db.get("githubIssues").value()
+  const storedIssues = await getStoredIssues()
   const rawGithubIssues = await fetchGithubIssues()
 
   console.log(`Found ${rawGithubIssues.length} issues`)
   console.log(`${storedIssues.length} issues already categorized and stored\n`)
-  for (const rawIssue of rawGithubIssues) {
-    const existingIssue = storedIssues.find(storedIssue => storedIssue.number === rawIssue.number)
-    if (!existingIssue) {
-      const category = await inquire(rawIssue)
-      const categorizedIssue = { ...rawIssue, category } as StoredGithubIssue
-      db.get("githubIssues").push(categorizedIssue).write()
-      console.log(`Issue ${rawIssue.number} categorized and stored\n`)
-    }
-  }
 
-  const categorizedIssues = db.get("githubIssues").value()
-  const frontendIssues = categorizedIssues.filter(issue => issue.category === "frontend")
-  const backendIssues = categorizedIssues.filter(issue => issue.category === "backend")
-  const databaseIssues = categorizedIssues.filter(issue => issue.category === "database")
-  const devopsIssues = categorizedIssues.filter(issue => issue.category === "devops")
-  const otherIssues = categorizedIssues.filter(issue => issue.category === "other")
+  const newIssues = await categorizeNewIssues(storedIssues, rawGithubIssues)
+  await storeNewIssues(newIssues)
 
-  console.log(`Total issues completed: ${categorizedIssues.length}`)
-  console.log(`Frontend issues: ${frontendIssues.length} (${Math.round((frontendIssues.length / categorizedIssues.length) * 100)}%)`)
-  console.log(`Backend issues: ${backendIssues.length} (${Math.round((backendIssues.length / categorizedIssues.length) * 100)}%)`)
-  console.log(`Database issues: ${databaseIssues.length} (${Math.round((databaseIssues.length / categorizedIssues.length) * 100)}%)`)
-  console.log(`Devops issues: ${devopsIssues.length} (${Math.round((devopsIssues.length / categorizedIssues.length) * 100)}%)`)
-  console.log(`Other issues: ${otherIssues.length} (${Math.round((otherIssues.length / categorizedIssues.length) * 100)}%)`)
+  countAndLogIssues([...storedIssues, ...newIssues])
 }
 
 main()
